c60b843e2398082d9b43578bceae46441bba800b
Leo Campos <leopcampos85@gmail.com>
2024-09-14 20:49:33 -0300

    Incluindo o Identity no projeto e criando a nova migration.

--- src/MinApiLivros/Context/AppDbContext.cs
+++ src/MinApiLivros/Context/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MinApiLivros.Entities;
 
 namespace MinApiLivros.Context;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
