7da047f3aadf46c6c9e76326399169bb60c5ede5
Leo Campos <leopcampos85@gmail.com>
2024-09-14 20:15:16 -0300

    Criando a entidade Livro, a classe de contexto e a classe de service.

---
+++ src/MinApiLivros/Context/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinApiLivros.Entities;
+
+namespace MinApiLivros.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Livro> Livros { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Livro>().HasData(
+            new Livro
+            {
+                Id = 1,
+                Titulo = "Clean Code",
+                Autor = "Robert C. Martin"
+            },
+            new Livro
+            {
+                Id = 2,
+                Titulo = "Clean Architecture",
+                Autor = "Robert C. Martin"
+            },
+            new Livro
+            {
+                Id = 3,
+                Titulo = "Programming Entity Framework Core",
+                Autor = "Julia Lerman"
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/MinApiLivros/Migrations/20240914231408_inicial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MinApiLivros.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Livros",
+                columns: new[] { "Id", "Autor", "Titulo" },
+                values: new object[,]
+                {
+                    { 1, "Robert C. Martin", "Clean Code" },
+                    { 2, "Robert C. Martin", "Clean Architecture" },
+                    { 3, "Julia Lerman", "Programming Entity Framework Core" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+        }
+    }
+}
